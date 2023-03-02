7fca26e2cd8c632b62fde841896020cd266f9be6
H-goto16 <h.goto.engineer@gmail.com>
2023-03-02 13:35:27 +0900

    feat: requirements and readme

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 # hand_detect
+
+## 環境構築
+
+```shell
+pip install -r requirements.txt
+```
+
+引数にn回左右を連続で検知するとreturnします。
+
+```python
+get_direction(n)
+```
--- finger_direction.py
+++ finger_direction.py
@@ -7,20 +7,19 @@ mp_hands = mp.solutions.hands
 # For webcam input:
 cap = cv2.VideoCapture(0)
 
-import cv2
-import mediapipe as mp
 
 mp_drawing = mp.solutions.drawing_utils
 mp_hands = mp.solutions.hands
 
-def get_direction(n):
+
+def get_direction(n: int) -> str:
     right_count = 0
     left_count = 0
     direction = None
     with mp_hands.Hands(
-        max_num_hands=2,
-        min_detection_confidence=0.5,
-        min_tracking_confidence=0.5) as hands:
+            max_num_hands=2,
+            min_detection_confidence=0.5,
+            min_tracking_confidence=0.5) as hands:
         while True:
             success, image = cap.read()
             if not success:
